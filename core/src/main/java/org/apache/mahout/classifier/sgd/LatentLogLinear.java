3ba148228ec130eb15ae7a4bca7f99ecd739f20b
Ted Dunning <ted.dunning@gmail.com>
2010-10-05 12:57:44 -0700

    Block sparse matrices are matrices sharded by row into small dense matrices.  The idea is to have a matrix that can be extended in the row direction.

---
+++ core/src/main/java/org/apache/mahout/classifier/sgd/LatentLogLinear.java
@@ -0,0 +1,8 @@
+package org.apache.mahout.classifier.sgd;
+
+/**
+ * Created by IntelliJ IDEA. User: tdunning Date: Oct 4, 2010 Time: 3:38:18 PM To change this
+ * template use File | Settings | File Templates.
+ */
+public class LatentLogLinear {
+}
---
+++ core/src/test/java/org/apache/mahout/classifier/sgd/LatentLogLinearTest.java
@@ -0,0 +1,13 @@
+package org.apache.mahout.classifier.sgd;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA. User: tdunning Date: Oct 4, 2010 Time: 6:10:31 PM To change this
+ * template use File | Settings | File Templates.
+ */
+public class LatentLogLinearTest {
+  @Test
+  public void testTrain() {
+  }
+}
